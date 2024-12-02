b16746467f7a1981881eb61935ffaf7a934c5795
puck3 <shipilovk04@gmail.com>
2024-12-02 23:24:56 +0300

    update config

--- src/core/config.py
+++ src/core/config.py
@@ -1,3 +1,16 @@
-from passlib.context import CryptContext
+import os
+from dotenv import load_dotenv
 
-password_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
+load_dotenv()
+
+DB_CONFIG = {
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DB_NAME"),
+    "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT"),
+    "min_size": int(os.getenv("POOL_MIN_CONN", 1)),
+    "max_size": int(os.getenv("POOL_MAX_CONN", 10)),
+}
+
+CRYPT_CONTEXT_CONFIG = {"schemes": ["pbkdf2_sha256"], "deprecated": "auto"}
