ee41a888fdd25973b91cbc5aec9a5ec843bff2d5
puck3 <shipilovk04@gmail.com>
2024-12-07 12:30:36 +0300

    update config

--- src/core/config.py
+++ src/core/config.py
@@ -4,13 +4,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DB_CONFIG = {
+    "dbname": os.getenv("DB_NAME"),
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASSWORD"),
-    "database": os.getenv("DB_NAME"),
     "host": os.getenv("DB_HOST"),
     "port": os.getenv("DB_PORT"),
-    "min_size": int(os.getenv("POOL_MIN_CONN", 1)),
-    "max_size": int(os.getenv("POOL_MAX_CONN", 10)),
 }
+
+POOL_MIN_CONN = int(os.getenv("POOL_MIN_CONN", 1))
+POOL_MAX_CONN = int(os.getenv("POOL_MAX_CONN", 10))
 
 CRYPT_CONTEXT_CONFIG = {"schemes": ["pbkdf2_sha256"], "deprecated": "auto"}
