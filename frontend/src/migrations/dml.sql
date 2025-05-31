07d35e31b8e27c1ffa3ff91c5cda8b4697b8fc27
Kirill Shipilov <shipilovk04@gmail.com>
2025-05-31 04:08:45 +0300

    remove db

--- frontend/src/migrations/dml.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users
-(email, login, password_hash, "role", first_name, last_name, birthdate)
-VALUES('admin@example.com', 'admin', '$pbkdf2-sha256$29000$PifkHEOIcW5NaY0xprQ2Jg$ta5FyWlaiyJo26jrNcdTE2tMFSQim5mbgH1oF1LhPa0', 'Администратор', NULL, NULL, NULL);
